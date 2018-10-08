6533d7ff0be0c26ed7ee07bd001d13811d7ac5ca
“Simaxa7” <simaxa7@mail.ru>
2018-10-08 17:02:47 +0300

    refactor: function 1,2,3,5,7,10

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
+const createEnumerableProperty = (propertyName) => {return propertyName};
+
+const createNotEnumerableProperty = (propertyName) => Symbol(propertyName);
+
+// не а не отправлять
+const createProtoMagicObject = ( ) => {
+	let object = new Function;
+	object.prototype = Object.getPrototypeOf(object);
+	return object;
+};
+
 const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+
+var countForAsync = 0;
+	const asyncIncrementor = () => {
+		countForAsync++;
+		return promise = new Promise((resolve, reject) => {
+			resolve(countForAsync);
+		})
+};
+
+const createIncrementer = (result) => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (countForAsync) => {
+	return promise = new Promise((resolve, reject) => {
+		setTimeout(function(){
+			resolve(countForAsync)}, 1000)
+	})
+};
+
+
 const getDeepPropertiesCount = () => {};
 const createSerializedObject = () => {};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => {return arr.sort()};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
@@ -24,3 +47,20 @@ exports.returnBackInSecond = returnBackInSecond;
 exports.getDeepPropertiesCount = getDeepPropertiesCount;
 exports.createSerializedObject = createSerializedObject;
 exports.sortByProto = sortByProto;
+
+
+// incrementor();
+// incrementor();
+// incrementor();
+
+// createProtoMagicObject();
+		// const propertyName = 'property';
+  //   const propertyValue = 'value';
+
+  //   const property = createEnumerableProperty(propertyName);
+  //   const object = {};
+
+  //   object[property] = propertyValue;
+
+  //   console.log(createEnumerableProperty(Object.keys(object).length, 1));
+  //   console.log(createEnumerableProperty(object[property], propertyValue));
